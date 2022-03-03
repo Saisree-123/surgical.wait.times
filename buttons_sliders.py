63fe9fd0df67c77c1b45a6da6e2ad35f9c3ffbaa
Saisree GR <saisree.gr@gmail.com>
2022-03-02 17:04:59 -0800

    creating branch plots

--- buttons_sliders.py
+++ buttons_sliders.py
@@ -3,7 +3,6 @@ import dash
 from dash import  dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
-
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout=app.layout = dbc.Container([   
